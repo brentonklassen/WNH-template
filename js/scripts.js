2c4e13213055484e538368675fdc1feaebafe075
Brenton Klassen <midwestmicro@live.com>
2014-11-03 16:53:41 -0600

    Added Bootstrap class to form buttons

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,6 @@ jQuery('#carousel-553052').carousel();
 jQuery('ul.nav > li.parent').addClass('dropdown');
 jQuery('ul.nav > li.parent > a').addClass('dropdown-toggle').attr('data-toggle','dropdown').append('<b class="caret"></b>');
 jQuery('ul.nav-child').addClass('dropdown-menu');
+
+// fix BreezingForms buttons
+jQuery('button.bfNextButton, button.bfPrevButton').addClass('btn');
